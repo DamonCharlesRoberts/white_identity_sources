29d2c303e30216b6ea6554625d8599a4d0bc5630
DamonCharlesRoberts <damon.charles.roberts@gmail.com>
2023-11-27 13:24:46 -0700

    stan code runs. I don't fully trust the stan code

--- src/models/model-fitting.R
+++ src/models/model-fitting.R
@@ -18,6 +18,8 @@ box::use(
 load(
     './data/temp/imputed_data.RData'
 )
+  #* Create empty model list object
+models <- list()
 
 # Fit models
 anes_2012_df <- na.omit(list_df[["anes_2012"]])
@@ -28,11 +30,12 @@ anes_2012 <- list(
     , K = 5
     , D = ncol(anes_2012_df[, -1:-3])
 )
-model[["2012"]] <- stan(
+models[["2012"]] <- stan(
     file = "./src/models/2012-model.stan"
     , data = anes_2012
     , chains = 6
     , cores = 5
     , iter = 2000
     , warmup = 1000
+    , model_name = "ANES 2012 LWD"
 )
